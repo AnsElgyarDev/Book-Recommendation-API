c990479f710b4ed40cc2f7bceca2821a9847d6b3
AnsElgyar <anselgyar27@gmail.com>
2026-09-21 23:16:46 +0300

    Creating Dto for Showing Books.

--- BookRecommendationAPI/Models/VolumeInfo.cs
+++ BookRecommendationAPI/Models/VolumeInfo.cs
@@ -5,5 +5,5 @@ public class VolumeInfo
     public string? Title { get; set; }
     public List<string>? Authors { get; set; }
     public string? Description { get; set; }
-    public int? PagesCount { get; set; }
+    public int? pagesCount { get; set; }
 }
