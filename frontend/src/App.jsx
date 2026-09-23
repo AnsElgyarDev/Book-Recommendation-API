78f3fffb2e52cad8decea2859d54289fb70a3ec2
AnsElgyar <anselgyar27@gmail.com>
2026-09-24 01:04:20 +0300

    Adding the Namespace for the Context of ToastPRovider.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/layout/NavBar";
 import SearchPage from "./pages/SearchPage";
 import FavoritesPage from "./pages/FavoritesPage";
+import { ToastProvider } from "./context/ToastContext";
 
 export default function App() {
   return (
-    <>
+    <ToastProvider>
+      {" "}
+      {}
       <NavBar />
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </>
+    </ToastProvider>
   );
 }
